10b50e825003d57ac8be1125a8f24a3b7893e52f
Nithi520 <chandrann6651@my.sduhsd.net>
2025-10-30 18:38:17 +0000

    base code for project

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
-// add javascript here
+//global variables
+// add a stop watch to see how long a round took, and look in book for guidelines
+let level, answer, score;
+const levelArr = document.getElementById("level");
+const scoreArr = [];
+Date.textContent = time();
+//add event listeners
+playBtn.addEventListener("click",play);
+guessBtn.addEventListener("click", makeGuess);
+
+function play(){
+    score = 0; // sets score to zero very new game 
+    playBtn.disabled = true; 
+    guessBtn.disabled = false; 
+    guess.disabled = false
+    //have to figure out give up on own
+    for(let i=0; i<levelArr.length; i++){
+       if(levelArr[i].checked){
+        level=levelArr[i].value; 
+       }
+       levelArr[i].disabled = true;
+    }
+ msg.textContent = "Guess a number from 1-" + level; 
+ answer = Math.floor(Math.random()*level)+1; 
+ guess.placeholder = answer;
+
+}
+
+function makeGuess(){
+    let userGuess = parseInt(guess.value);
+    if(isNaN(userGuess) || userGuess<1 || userGuess > level){
+        msg.textContent = "Enter a VALID #1-" + level; 
+        return;
+    }
+    score++; //valid guess add 1 to score
+    if(userGuess < answer){
+        msg.textContent = "Too low, try again";
+    }
+    else if(userGuess > answer){
+        msg.textContent = "Too high, try again";
+    }
+    else{
+        msg.textContent = "You got it, it took you " + score + " tries. Press play to play again";
+        updateScore();
+
+    }
+}
+
+function reset(){
+    guessBtn.disabled = true; 
+    guess.disabled = true; 
+    guess.value = "";
+    guess.placeholder = ""; 
+    playBtn.disabled = false; 
+    for(let i=0; i<levelArr.length; i++){
+        levelArr[i].disabled = false;
+    }
+
+}
+
+function updateScore(){
+    scoreArr.push(score);
+    scoreArr. sort((a,b) => a-b);
+    let lb = document.getElementsByName("leaderboard")
+    wins.textContent = "Total wins:" + scoreArr.length;
+    let sum = 0;
+    for(let i=0; i<scoreArr.length; i++){
+        sum += scoreArr[i];
+        if(i<lb.length){
+            lb[i].textContent = scoreArr[i];
+        }
+    }
+    let avg = sum/scoreArr.length;
+    avgScore.textContent = "Average Score:" +avg.toFixed(2);
+}
+
+function time(){
+    let d = new Date();
+    // concatenate a string with all the date info 
+    d = d.getFullYear() + " " + d.getTime() 
+    return d;
+
+}
